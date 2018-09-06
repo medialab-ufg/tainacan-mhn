e5c3eeb91f040c79fefce17974e50ab892269ab3
Alvino Rodrigues <alvino.digo@gmail.com>
2018-09-06 18:12:05 -0300

    tirando posts da home

--- front-page.php
+++ front-page.php
@@ -39,56 +39,10 @@
 						</li>
 					</ul>
 				</div>
-				<?php get_template_part('template-parts/loop', 'singular'); ?>
+				<?php //get_template_part('template-parts/loop', 'singular'); ?>
 			</div><!-- /#content -->
 		</div>
-		
 	</div><!-- /.row -->
-	
-	<?php $noticias = new WP_Query('post_type=post'); ?>
-	
-	<?php if ($noticias->have_posts()): ?>
-	
-		<div class="row">
-			<div class="col-sm margin-one-column">
-				<div class="box-noticias">
-					<div class="tainacan-title">
-						<div class="border-bottom mb-5 border-jelly-bean tainacan-title-page" style="border-width: 2px !important;">
-							<ul class="list-inline mb-1">
-								<li class="list-inline-item text-midnight-blue font-weight-bold title-page">
-									Notícias
-								</li>
-								<li class="list-inline-item float-right title-back"><a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">Ver mais</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<div class="row justify-content-center">
-						<div class="col-lg-9">
-							<ul class="box-noticias__lista">
-								
-								<?php while ($noticias->have_posts()): $noticias->the_post(); ?>
-								
-									<li>
-										<h3 class="box-noticias__titulo"><?php the_title(); ?></h3>
-										<p><?php the_excerpt(); ?></p>
-										<div class="box-noticias__mais">
-											<a href="<?php the_permalink(); ?>">Leia mais...</a>
-										</div>
-									</li>
-								
-								<?php endwhile; ?>
-								
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-	<?php endif; ?>
-	<?php wp_reset_postdata(); ?>
-	
 </div>
 
 
