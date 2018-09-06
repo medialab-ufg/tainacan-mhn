209bf2c8cb8603becf44d568fdf3ab720afe0a5d
Alvino Rodrigues <alvino.digo@gmail.com>
2018-09-06 18:09:16 -0300

    Lista de destaques concluida

--- front-page.php
+++ front-page.php
@@ -7,18 +7,38 @@
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menuBellowBanner' ); ?>
 
-<div class="container-fluid mt-5 max-large">
+<div class="container-fluid margin-top-95 max-large">
 	<div class="row">
 
 		<div class="col-sm margin-one-column">
 			<div id="content" role="main">
-				<!-- <div class="destaques-list">
+				<div class="highlights-list margin-bottom-25">
+					<div class="highlights-list__title-box">
+						<h3 class="highlights-list__title">Exposições em destaque</h3>
+						<span class="highlights-list__sub">Subtítulo, se necessário for.</span>
+					</div>
+
 					<ul>
 						<li>
-							
+							<a href="#">
+								<img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/fke/colecao_001.jpg'; ?>" alt="Coleção">
+								<strong>Aspectos do Brasil republicano</strong>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/fke/colecao_001.jpg'; ?>" alt="Coleção">
+								<strong>Retratos do Império</strong>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/fke/colecao_001.jpg'; ?>" alt="Coleção">
+								<strong>Paisagens cariocas</strong>
+							</a>
 						</li>
 					</ul>
-				</div> -->
+				</div>
 				<?php get_template_part('template-parts/loop', 'singular'); ?>
 			</div><!-- /#content -->
 		</div>
