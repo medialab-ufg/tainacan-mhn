b94db4382518e561be249f9769bf22cd23661761
Alvino Rodrigues <alvino.digo@gmail.com>
2018-09-11 18:18:41 -0300

    Implementando barra de acessibilidade

--- header.php
+++ header.php
@@ -11,18 +11,11 @@
 </head>
 <body <?php body_class(); ?> data-theme="mhn-theme-page">
     <!-- MENU DE ACESSIBILIDADE -->
-    <ul class="accessibility-shortcuts" role="menubar">
-        <li role="menuitem"><a href="#content" accesskey="c">Ir para o conteúdo <span>c</span></a></li>
-        <li role="menuitem"><a href="#menu-menu-1" accesskey="m">Ir para o menu <span>m</span></a></li>
-        <li role="menuitem"><a href="#search-box__search" accesskey="b">Ir para a busca <span>b</span></a></li>
-        <li role="menuitem"><a href="#footer" accesskey="r">Ir para o rodapé <span>r</span></a></li>
-    </ul>
-
-    <ul class="accessibility-options" role="menubar">
-        <li role="menuitem"><button type="button" class="button-text-minus" accesskey="5">A-</button></li>
-        <li role="menuitem"><button type="button" class="button-text-default" accesskey="6">A</button></li>
-        <li role="menuitem"><button type="button" class="button-text-plus" accesskey="7">A+</button></li>
-        <li role="menuitem"><button type="button" class="button-high-contrast" accesskey="8">Alto Contraste</button></li>
+    <ul class="accessibility-shortcuts accessibility-shortcuts--hidden" role="menubar">
+        <li role="menuitem"><a href="#content" accesskey="c"><span>c</span> Ir para o conteúdo</a></li>
+        <li role="menuitem"><a href="#menu-menu-1" accesskey="m"><span>m</span> Ir para o menu</a></li>
+        <li role="menuitem"><a href="#search-box__search" accesskey="b"><span>b</span>Ir para a busca</a></li>
+        <li role="menuitem"><a href="#footer" accesskey="r"><span>r</span>Ir para o rodapé</a></li>
     </ul>
 
     <!-- BARRA DO GOVERNO -->
@@ -31,4 +24,28 @@
             <li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED"><a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a></li> 
             <li><a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a></li>
         </ul>
+    </div>
+
+    <div class="accessibility-bar">
+        <div class="accessibility-bar__container">
+            <ul class="accessibility-shortcuts" role="menubar">
+                <li role="menuitem"><a href="#content" accesskey="c"><span>c</span> Ir para o conteúdo</a></li>
+                <li role="menuitem"><a href="#menu-menu-1" accesskey="m"><span>m</span> Ir para o menu</a></li>
+                <li role="menuitem"><a href="#search-box__search" accesskey="b"><span>b</span>Ir para a busca</a></li>
+                <li role="menuitem"><a href="#footer" accesskey="r"><span>r</span>Ir para o rodapé</a></li>
+            </ul>
+
+            <ul class="accessibility-options" role="menubar">
+                <li role="menuitem">
+                    <span>Fonte</span>
+                    <button type="button" class="button-text-minus" accesskey="5">A-</button>
+                    <button type="button" class="button-text-default" accesskey="6">A</button>
+                    <button type="button" class="button-text-plus" accesskey="7">A+</button>
+                </li>
+                <li role="menuitem">
+                    <span>Contraste</span>
+                    <button type="button" class="button-high-contrast" accesskey="8">Alto Contraste</button>
+                </li>
+            </ul>
+        </div>
     </div>
