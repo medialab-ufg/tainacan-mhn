d4442fec4b61a846a986e2f0b351be3bff19c0e1
Alvino Rodrigues <alvino.digo@gmail.com>
2018-09-19 10:36:00 -0300

    Correcao na lista de itens mais visitados

--- front-page.php
+++ front-page.php
@@ -12,13 +12,15 @@
 		<div class="col-sm margin-one-column">
 			<div id="content" role="main">
 				<div class="highlights-list margin-bottom-25">
-					<div class="highlights-list__title-box">
-						<h3 class="highlights-list__title">Exposições em destaque</h3>
-						<span class="highlights-list__sub">Subtítulo, se necessário for.</span>
+					<div class="margin-two-column">
+						<div class="highlights-list__title-box">
+							<h3 class="highlights-list__title">Exposições em destaque</h3>
+							<span class="highlights-list__sub">Subtítulo, se necessário for.</span>
+						</div>
 					</div>
-					
+
 					<?php $expoUrl = 'reserva-tecnica/#/?fetch_only%5B0%5D=thumbnail&fetch_only%5B1%5D=creation_date&fetch_only%5B2%5D=author_name&fetch_only%5B3%5D=title&fetch_only%5B4%5D=description&fetch_only%5Bmeta%5D%5B0%5D=0&view_mode=masonry&perpage=48&paged=1&order=DESC&orderby=date&taxquery%5B0%5D%5Btaxonomy%5D=tnc_tax_85161&taxquery%5B0%5D%5Bterms%5D={{TERM_ID}}&taxquery%5B0%5D%5Bcompare%5D=IN'; ?>
-					
+
 					<ul>
 						<li>
 							<a href="<?php echo site_url( str_replace('{{TERM_ID}}', 21291, $expoUrl) ); ?>">
@@ -42,11 +44,11 @@
 				</div>
 				<?php 
 				global $MHNViewCounter;
-				$popularItems = $MHNViewCounter->get_items(3);
+				$popularItems = $MHNViewCounter->get_items(4);
 				?>
-				
+
 				<?php if ($popularItems->have_posts()) : ?>
-				
+
 					<div class="highlights-list margin-bottom-25">
 						<div class="margin-two-column">
 							<div class="highlights-list__title-box">
@@ -62,7 +64,7 @@
 								
 									<li>
 										<a href="<?php the_permalink(); ?>">
-											<?php the_post_thumbnail('tainacan-medium-full'); ?>
+											<?php the_post_thumbnail('tainacan-medium'); ?>
 											<strong><?php the_title(); ?></strong>
 										</a>
 									</li>
@@ -72,9 +74,9 @@
 							</ul>
 						</div>
 					</div>
-					
+
 				<?php endif; ?>
-				
+
 				<?php //get_template_part('template-parts/loop', 'singular'); ?>
 			</div><!-- /#content -->
 		</div>
