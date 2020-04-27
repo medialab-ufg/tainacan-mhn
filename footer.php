c64d0a7e579cbc87304e6f558e15cfc4f2ab6779
mateuswetah <mateus.m.luna@gmail.com>
2020-04-27 09:47:23 -0300

    Atualiza footer com versao clara da parte das logos.

--- footer.php
+++ footer.php
@@ -1,25 +1,27 @@
 <?php if(!is_404()) : ?>
-    <footer id="footer" class="container-fluid p-4 p-sm-5 mt-5 tainacan-footer" style="padding-bottom: 0 !important;">
-        <?php if ( is_active_sidebar( 'tainacan-sidebar-footer' ) ) { ?>
-            <div class="row">
-                <div class="col-12 col-lg">
-                    <ul class="p-4 d-lg-flex justify-content-md-between mb-md-0">
-                        <?php dynamic_sidebar('tainacan-sidebar-footer'); ?>
-                    </ul>
+    <footer id="footer">
+        <div class="container-fluid p-4 p-sm-5 mt-5 tainacan-footer" style="padding-bottom: 0 !important;">
+            <?php if ( is_active_sidebar( 'tainacan-sidebar-footer' ) ) { ?>
+                <div class="row">
+                    <div class="col-12 col-lg">
+                        <ul class="p-4 d-lg-flex justify-content-md-between mb-md-0">
+                            <?php dynamic_sidebar('tainacan-sidebar-footer'); ?>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        <?php } ?>
-        <hr class="bg-scooter" style="background-color: #c34250 !important;"/>
-        <div class="row p-4 tainacan-mhn-footer--barra-logos text-white text-center">
-            <div class="col-12 col-sm mb-5 mb-md-0"><img src="<?php echo get_template_directory_uri() ?>/assets/images/logo-footer.svg" alt=""></div>
-            <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/aanh.png" alt=""></div>
-            <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/lgo/mhn.png" alt=""></div>
-            <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/sbm.png" alt=""></div>
-            <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/ibram.png" alt=""></div>
-            <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/logo-ministeriodacultura.png" alt=""></div>
-            <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/nome-governo-federal.png" alt=""></div>
+            <?php } ?>
         </div>
-
+        <div class="container-fluid p-4 p-sm-5 logos-footer">
+            <div class="row p-4 tainacan-mhn-footer--barra-logos text-white text-center">
+                <div class="col-12 col-sm mb-5 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/tainacan.png" alt="Marca do Tainacan"></div>
+                <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/aanh.png" alt="Marca da AANH"></div>
+                <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/lgo/mhn.png" alt="Marca do Museu Histórico Nacional"></div>
+                <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/sbm.png" alt="Marca do Sistema Brasileiro de Museus"></div>
+                <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/ibram.png" alt="Marca do Instituto Brasileiro de Museus"></div>
+                <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/logo-ministerioturismo.png" alt="Marca do Ministério do Turismo"></div>
+                <div class="col-12 col-sm mb-3 mb-md-0"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/nome-governo-federal.png" alt="Marca do Governo Federal"></div>
+            </div>
+        </div> 
     </footer>
 <?php endif; ?>
 <?php wp_footer(); ?>
